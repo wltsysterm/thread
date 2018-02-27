6d19616d44f81c93a3930e5e8f84cf412c5f6a32
魏霖涛 <3263138624@qq.com>
2018-02-27 11:04:18 +0800

    多线程notify wait案例

---
+++ src/main/java/com/wlt/App.java
@@ -0,0 +1,21 @@
+package com.wlt;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
