16b319b54bf98acd3402671bfeb7cd74f6124b1c
魏霖涛 <3263138624@qq.com>
2018-02-27 15:19:15 +0800

    单线程和多线程结束判断

--- src/main/java/com/wlt/App.java
+++ src/main/java/com/wlt/App.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Vector;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.*;
 
 /**
  * Hello world!
@@ -13,9 +13,36 @@ import java.util.concurrent.ConcurrentMap;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    private static ExecutorService pool = new ThreadPoolExecutor(1,1,60, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<Runnable>());
+    public static void main( String[] args ) throws InterruptedException {
         System.out.println( "Hello World!" );
+
+        wltThread thread = new wltThread();
+        pool.execute(thread);
+        pool.shutdown();
+        while(true){
+            if(pool.isTerminated()){
+                break;
+            }
+        }
+        System.out.println("主线程结束");
+        System.out.println("=====================");
+        Thread thread1 = new Thread(new wltThread());
+        thread1.start();
+        thread1.join();
+        System.out.println("单独主线程结束");
     }
 
+}
+class wltThread implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("准备休眠5s");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("自线程运行完毕");
+    }
 }
